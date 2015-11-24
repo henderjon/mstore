b9e1a5b2d3fc8e4b814ee1425e5764f59e1eda3a
henderjon <jhenderson@capstonedigital.com>
2015-11-24 12:27:40 -0600

    wip

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+/*
+Package mstore provides a means for serializing data in a format similar to raw
+email or http requests. Messages compose headers and a body. The advantage to
+this is Messages are both human readable and machine parsable.
+*/
 package main
 
 import (
@@ -5,6 +10,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/mail"
@@ -13,41 +19,38 @@ import (
 
 const EOL = "\r\n"
 
+// messages have two parts: Meta and Body. Messages very closely resemble a raw
+// email message or raw HTTP request with the absence of a few distinctive
+// features. Namely, there are no required headers. In the case of email `From`
+// and in the case of HTTP requests the opener
 type Message struct {
-	Header http.Header
-	Body   *bufio.ReadWriter
+	Meta http.Header
+	Body *bufio.ReadWriter
 }
 
-func NewMessage() Message {
+// create a new message
+func NewMessage() *Message {
 	b := bytes.Buffer{}
-	return Message{
-		make(http.Header),
-		bufio.NewReadWriter(
-			bufio.NewReader(&b),
-			bufio.NewWriter(&b),
-		),
+	rw := bufio.NewReadWriter(bufio.NewReader(&b), bufio.NewWriter(&b))
+	h := http.Header{}
+	return &Message{
+		h,
+		rw,
 	}
 }
 
-func main() {
-	m := NewMessage()
-	m.Header.Add("test-header", "test-value")
-	m.Body.Write([]byte(`this is a long string of stuff`))
-	m.Body.Flush()
-	m.Put(os.Stdout)
-
-	fmt.Println("#----------------------#")
-
-	by := bytes.Buffer{}
-	by.WriteString("Testing: this is a value\r\nAnother: value\r\n\r\nThis is a test body")
-	m = Get(&by)
-	m.Put(os.Stdout)
+// hide the Message.Body.Flush() from the API
+func (p *Message) Write(b []byte) (int, error) {
+	i, err := p.Body.Write(b)
+	p.Body.Flush()
+	return i, err
 }
 
-func (p Message) Put(w io.Writer) {
+// write to w, ending with two blank lines
+func (p *Message) SerializeTo(w io.Writer) {
 	var err error
 
-	if err = p.Header.WriteSubset(w, nil); err != nil {
+	if err = p.Meta.WriteSubset(w, nil); err != nil {
 		log.Fatal(err)
 	}
 
@@ -62,7 +65,8 @@ func (p Message) Put(w io.Writer) {
 	w.Write([]byte(EOL))
 }
 
-func Get(r io.Reader) Message {
+// read from r, parsing into a Message{ Meta, Body }
+func (p *Message) DeserializeFrom(r io.Reader) {
 	mm, err := mail.ReadMessage(r)
 
 	if err != nil {
@@ -82,9 +86,33 @@ func Get(r io.Reader) Message {
 
 	h := http.Header(mm.Header)
 
-	return Message{
-		h,
-		buf,
-	}
+	p.Meta = h
+	p.Body = buf
 
+}
+
+func main() {
+
+	f, _ := os.Open("test.mbox")
+	m := NewMessage()
+	m.DeserializeFrom(f)
+	fmt.Println(m.Meta.Get("X-Powered-By"))
+
+	b, _ := ioutil.ReadAll(m.Body)
+	fmt.Println(string(b))
+
+	// m.SerializeTo(os.Stdout)
+
+	// m := NewMessage()
+	// m.Meta.Add("test-header", "test-value")
+	// m.Write([]byte(`this is a long string of stuff`))
+	// m.Write([]byte(`with even more after that`))
+	// m.Put(os.Stdout)
+
+	// fmt.Println("#----------------------#")
+
+	// by := &bytes.Buffer{}
+	// by.WriteString("Testing: this is a value\r\nAnother: value\r\n\r\nThis is a test body")
+	// m.Get(by)
+	// m.Put(os.Stdout)
 }
